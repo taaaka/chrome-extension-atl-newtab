5d308e29dd46181ef15b8986552158674391e180
taaaka <Rw.taaaka@gmail.com>
2018-12-03 21:59:00 +0900

    [add] webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'dist/js')
+  }
+};
