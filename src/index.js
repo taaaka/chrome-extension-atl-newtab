eae702057e083089396ce9d0458b17a6436a3d01
taaaka <Rw.taaaka@gmail.com>
2018-12-04 22:24:22 +0900

    [modify] add targetBlank

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('Hello, world!!');
+const convertLinks = (query = '') => {
+  document.querySelectorAll(query).forEach((el) => {
+    el.setAttribute('target', '_blank');
+  });
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  convertLinks('.new');
+  console.info('[Atl newtab] convert finished!!');
+});
